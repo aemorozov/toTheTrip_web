3545979f3f526dd4a2b344e9210fe60f69862b82
aemorozov <mr.aemorozov@gmail.com>
2026-02-09 22:12:58 +0200

    HeroBlock: update colors to light version

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Rubik } from "next/font/google";
 import "./globals.css";
+import Navigation from "../components/Navigation/Navigation";
 
 const rubik = Rubik({
   subsets: ["latin"],
@@ -20,7 +21,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={rubik.className}>{children}</body>
+      <body className={rubik.className}>
+        <header>
+          <Navigation />
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,3 +1,12 @@
-export function Navigation() {
-  return <nav></nav>;
+import styles from "./Navigation.module.css";
+
+export default function Navigation() {
+  return (
+    <nav className={styles.navigation}>
+      <div className={styles.logoText}>
+        <p className={styles.logo}>toTheTrip.app</p>
+        <p className={styles.description}>we already found cheapest flights.</p>
+      </div>
+    </nav>
+  );
 }
